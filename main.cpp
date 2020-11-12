1b4534910f1b37bd8e14052bcdb54b8a743db7a0
Srikahn <49712202+Srikahn@users.noreply.github.com>
2020-11-12 13:14:23 +0100

    Instantiate a comet

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+#include "Comet.h"
+
 int main()
 {
-   std::cout << "Hello, World!" << std::endl;
-   return 0;
+	std::cout << "Hello, World!" << std::endl;
+
+	Comet comet(0, 0, "x", true, 1);
+	std::cout << "Hey, I just saw a comet pass! It was beautiful." << std::endl;
+
+	return 0;
 }
