b4ac3b93ef100a4b833b75989c32837ee4a2a6f6
Srikahn <49712202+Srikahn@users.noreply.github.com>
2020-11-19 18:21:32 +0100

    Cleaning up Comet

--- Comet.cpp
+++ Comet.cpp
@@ -7,38 +7,3 @@ Comet::Comet(int x, int y, char axis, bool direction, float speed) : Projectile(
 
 Comet::~Comet() {}
 // ------------------------------------
-
-/*
-void Comet::tick(float deltaTime)
-{
-	// The amount of move available this turn
-	float move = deltaTime * m_speed + m_moveRemaining;
-
-	// Let's get the integer part of the move to see what movement is actually performed this tick, and put the rest in moveRemaining
-	float intMove = 0;
-	m_moveRemaining = modff(move, &intMove);
-
-	// Perform the move in the programmed direction
-	int newPosX = m_positionX;
-	int newPosY = m_positionY;
-	int orientation = 0;
-	if(m_axis == "x")
-	{
-		orientation = (m_direction) ? 1 : -1;
-		newPosX += intMove * orientation;
-	}
-	else
-	{
-		orientation = (m_direction) ? 1 : -1;
-		newPosY += intMove * orientation;
-	}
-
-	moveTo(newPosX, newPosY);
-}
-
-void Comet::collideWith(Actor& collider)
-{
-	// The comet is immediately destroyed when collided
-	destroy();
-}
-*/
